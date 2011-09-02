eaaef446e53e719ee01154a99ea1fe9af35556c6
Takumi Kinjo <takumi.kinjo@gmail.com>
2011-09-02 10:28:29 +0900

    add a UITableViewController

---
+++ NavigationBasedAppFirst/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  NavigationBasedAppFirst
+//
+//  Created by 金城 拓実 on 11/09/02.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DetailViewController : UITableViewController {
+    
+}
+
+@end
